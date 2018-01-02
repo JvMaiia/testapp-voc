3c893d3619466ca5f5ccfa77ec672a8cd63d862e
jvmaia <joaovmferreira@gmail.com>
2018-01-02 19:26:20 -0200

    add a date view

--- testapp/app.py
+++ testapp/app.py
@@ -1,6 +1,8 @@
 import android
 from android.widget import Button, LinearLayout, RelativeLayout, TextView, EditText
 from android.view import Gravity
+from java.util import Calendar
+from java.text import SimpleDateFormat
 
 class ButtonClick(implements=android.view.View[OnClickListener]):
     def __init__(self, callback, *args, **kwargs):
@@ -16,26 +18,39 @@ class MainApp:
         self._activity = android.PythonActivity.setListener(self)
 
     def onCreate(self):
-        vlayout = LinearLayout(self._activity)
-        vlayout.setOrientation(LinearLayout.VERTICAL)
+        self.vlayout = LinearLayout(self._activity)
+        self.vlayout.setOrientation(LinearLayout.VERTICAL)
 
         self.textbox = EditText(self._activity)
-        vlayout.addView(self.textbox)
+        self.vlayout.addView(self.textbox)
 
         self.update_text_button = Button(self._activity)
         self.update_text_button.setText('update text')
         self.update_text_button.setOnClickListener(ButtonClick(self.update_text))
-        vlayout.addView(self.update_text_button)
+        self.vlayout.addView(self.update_text_button)
 
         self.text_result = TextView(self._activity)
         self.text_result.setText('test')
-        vlayout.addView(self.text_result)
-        
-        self._activity.setContentView(vlayout)
+        self.vlayout.addView(self.text_result)
+
+        self.change_view_button = Button(self._activity)
+        self.change_view_button.setText('change view')
+        self.change_view_button.setOnClickListener(ButtonClick(self.change_view))
+        self.vlayout.addView(self.change_view_button)
+
+        self._activity.setContentView(self.vlayout)
 
     def update_text(self):
         text = self.textbox.getText()
         self.text_result.setText(text)
+
+    def change_view(self):
+        self.vlayout.removeAllViews()
+        self.vlayout.addView(self.text_result)
+        calendar = Calendar.getInstance()
+        dateformat = SimpleDateFormat('yyyy / MM / dd')
+        now = dateformat.format(calendar.getTime())
+        self.text_result.setText(now)
 
 def main():
     MainApp()
